716b4905d2f7924c682cc997589fa6a26196fbbd
Ozobia Nwadibia <Ozobia.cn3@gmail.com>
2019-11-08 18:58:28 -0600

    Adding w3.css and updating product.css

--- assets/scripts/product.js
+++ assets/scripts/product.js
@@ -1,3 +1,13 @@
+// toggle function for the links on navbar
+function toggle() {
+  const navbar = document.getElementById("nav-bar");
+  if (navbar.className === "nav-bar") {
+    navbar.className += " responsive";
+  } else {
+    navbar.className = "nav-bar";
+  }
+}
+
 //function to check form input validation
 function checkFormInput() {
   const emailInput = document.getElementById("email").value;
